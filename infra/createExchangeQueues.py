eb055fd706602edaf57f80249d19fb21a1c01d61
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-05-28 00:21:55 +0300

    Added python script for automagically creating exchange, bindings and queues

---
+++ infra/createExchangeQueues.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+import pika
+
+USER = "user"
+PASSWORD = "local-password"
+HOST = "localhost"
+
+USER_MANAGEMENT_QUEUE = "user_management"
+USER_MANAGEMENT_ROUTING_KEY = "jobportal.user.*.events"
+
+POSTINGS_MANAGEMENT_QUEUE = "postings_management"
+POSTINGS_MANAGEMENT_ROUTING_KEY = "jobportal.postings.*.events"
+LOGGING_EXCHANGE = "logging"
+
+credentials = pika.PlainCredentials(USER, PASSWORD)
+parameters = pika.ConnectionParameters(HOST, 5672, '/', credentials)
+
+print(f'Connecting to RabbitMQ host: {HOST}:5672 with user: {USER}')
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.exchange_declare(exchange=LOGGING_EXCHANGE, exchange_type='topic', durable=True)
+channel.queue_declare(queue=USER_MANAGEMENT_QUEUE, exclusive=False, durable=True)
+channel.queue_bind(USER_MANAGEMENT_QUEUE, LOGGING_EXCHANGE, routing_key=USER_MANAGEMENT_ROUTING_KEY)
+
+channel.queue_declare(queue=POSTINGS_MANAGEMENT_QUEUE, exclusive=False, durable=True)
+channel.queue_bind(POSTINGS_MANAGEMENT_QUEUE, LOGGING_EXCHANGE, routing_key=POSTINGS_MANAGEMENT_ROUTING_KEY)
+
+connection.close()
