0f49477a8373b382f62b4c98c5fb6a1cc1b3f1c7
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-05-29 22:47:24 +0300

    Read image value from values.yaml which is overriden in skaffold.yaml. Read configs from env variables

--- userManagementService/config/runtime.exs
+++ userManagementService/config/runtime.exs
@@ -1,5 +1,5 @@
 import Config
-# System.fetch_env!("MY_APP_SECRET_KEY")
-config :user_management, :db_host, "mongodb"
-config :user_management, :app_secret_key, "secret"
-config :user_management, :event_url, "user:local-password@rabbitmq"
+
+config :user_management, :db_host, System.fetch_env!("MONGODB_HOST")
+config :user_management, :app_secret_key, System.fetch_env!("APP_SECRET_KEY")
+config :user_management, :event_url, "#{System.fetch_env!("RABBITMQ_USER")}:#{System.fetch_env!("RABBITMQ_PASSWORD")}@#{System.fetch_env!("RABBITMQ_HOST")}"
