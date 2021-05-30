9d1abc94e8b96b00121bec7a48ad164205e0a90d
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-05-30 11:50:12 +0300

    Add default values in case environment variables not found

--- jobsPortalService/config/runtime.exs
+++ jobsPortalService/config/runtime.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :portal_management, :api_port, 4000
-config :portal_management, :user_management_service_url, System.fetch_env!("USER_MANAGEMENT_SERVICE_URL")
-config :portal_management, :posting_management_service_url, System.fetch_env!("POSTING_MANAGEMENT_SERVICE_URL")
-config :portal_management, :statistics_service_url, System.fetch_env!("STATISTICS_SERVICE_URL")
-config :portal_management, :origin, ["http://localhost:4200", System.fetch_env!("CORS")]
+config :portal_management, :user_management_service_url, System.get_env("USER_MANAGEMENT_SERVICE_URL", "http://localhost:4000")
+config :portal_management, :posting_management_service_url, System.get_env("POSTING_MANAGEMENT_SERVICE_URL", "http://localhost:3000")
+config :portal_management, :statistics_service_url, System.get_env("STATISTICS_SERVICE_URL", "http://localhost:9000")
+config :portal_management, :origin, ["http://localhost:4200", System.get_env("CORS", "http://localhost:4200")]
